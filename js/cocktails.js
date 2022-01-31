549acb3940e71c57e33b44609bf71c683eac4f1c
Solange ormeno <zol22@Solanges-MacBook-Air.local>
2022-01-31 18:58:46 -0500

    first commit

---
+++ js/cocktails.js
@@ -0,0 +1,51 @@
+function getRandomCocktail(){
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function(data) {
+        //console.log(data);
+        displayRandomCocktail(data);
+      });
+}
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+}
+
+getRandomCocktail();
+
+function displayRandomCocktail(cocktail){
+  console.log(cocktail.drinks[0]);
+
+  // drink name
+  let drinkSection = document.querySelector('#drink-section');
+  let drinkName = document.createElement('h2');
+  drinkName.innerHTML = cocktail.drinks[0].strDrink;
+  drinkSection.appendChild(drinkName);
+
+  // image
+  let img = document.createElement('img');
+  img.src = cocktail.drinks[0].strDrinkThumb;
+  drinkSection.appendChild(img);
+
+  //ingredient
+  for (let i=1; i<16; i++){
+    if(cocktail.drinks[0][`strIngredient${i}`] == null || cocktail.drinks[0][`strIngredient${i}`] == '' ){
+      break;
+    }
+    let ingredient = document.createElement('ul');
+    ingredient.innerHTML = cocktail.drinks[0][`strMeasure${i}`] + ':' + cocktail.drinks[0][`strIngredient${i}`];
+    drinkSection.appendChild(ingredient);
+}
+    //instruction
+    let instruction = document.createElement('p');
+    instruction.innerHTML = cocktail.drinks[0].strInstructions;
+    drinkSection.appendChild(instruction);
+  }
